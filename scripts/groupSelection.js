43e7a0a1c64b9ca6b0d6c8a795ad5d4abe862550
Jacky <ccc1020048076@gmail.com>
2024-09-04 15:19:36 +0930

    Jiaqi-Fix: Seperate js according to function, css file name fixed

---
+++ scripts/groupSelection.js
@@ -0,0 +1,16 @@
+// groupSelection.js
+// Select all group items
+var groupItems = document.querySelectorAll(".group-item");
+
+// Add click event listener to each group item
+groupItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+    // Remove 'selected' class from all group items
+    groupItems.forEach(function (group) {
+      group.classList.remove("selected");
+    });
+
+    // Add 'selected' class to the clicked group item
+    item.classList.add("selected");
+  });
+});
