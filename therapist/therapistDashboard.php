2050e30684cfd98339cfefe1d52f0effc121919c
MaxKim <maxkim0314@gmail.com>
2024-10-03 01:40:10 +0930

    therapistDashboard start

--- therapist/therapistDashboard.php
+++ therapist/therapistDashboard.php
@@ -1,3 +1,17 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "caredb"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
